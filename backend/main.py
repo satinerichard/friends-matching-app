51cd793f3d8c4ae662c8a00b569a914cb49a25e6
Satine Richard <satinerichard@Satines-MacBook-Air.local>
2026-02-07 17:19:13 -0500

    added buttons

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,11 @@ from typing import List
 
 app = FastAPI()
 
+MOCK_DATABASE_USERS = []
+
 #tell what a user looks like
 class User(BaseModel):
+    id: int = None
     username: str
     email: str
     full_name: str
@@ -15,24 +18,47 @@ class User(BaseModel):
     age: int
     interests: List[str]
 
-class Location(BaseModel):
-    longitude: float
-    latitude: float
+#class Location(BaseModel):
+ #   longitude: float
+  #  latitude: float
+
 
 @app.get("/")
 def root():
-    return {'message': 'Hello World'}
+    return {'message': 'Friender'}
 
-@app.get("/users")
-def read_users():
-    return MOCK_DATABASE_USERS
+@app.post("/add_user")
+def add_user(user: User):
+    MOCK_DATABASE_USERS.append(user)
+    return {'message': 'User added!' , 'user':user }
 
-@app.post("/user")
-def create_users(user_data: User):
+@app.get("/people")
+def get_people(my_name: str):
+    # show all users except yourself
+    others =[]
+    for user in MOCK_DATABASE_USERS:
+        if user.username != my_name:
+            others.append(user)
+    return {'people': others}
 
-    MOCK_DATABASE_USERS.append(user_data)
-    return {'message': 'User created successfully', 'user': user_data}
 
-MOCK_DATABASE_USERS = []
+# create the like/dislike buttons 
+actions = []
+
+class Action(BaseModel):
+    on_who: str
+    liked_by: str
+    liked: bool
+
+@app.post("/button")
+def button(action: Action):
+    actions.append(action)
+    return {'message': 'Action recorded!' , 'action': action } 
+    print(f"{action.on_who} got {'❤️' if action.liked else '❌'}")
+    
+    if action.liked:
+        return {"message": "❤️ LIKED!"}
+    return {"message": "❌ PASSED"}
+
 
 
